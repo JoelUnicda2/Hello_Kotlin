d528ab4bb510ad5008c327a973f2e76a64c62e74
JoelUnicda2 <joel.jimenez@unicda.edu.do>
2025-01-31 07:44:23 -0400

    Practica Greetings, Kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -3,8 +3,14 @@ import java.util.Calendar
 fun main(args: Array<String>) {
     println("Hello, ${args[0]}!")
 
+    greetingMain(args)
+
     dayoftheweek()
+    timeinanotherway(args)
 }
+
+
+
 fun dayoftheweek() {
 
     println("What day is today?")
@@ -21,4 +27,30 @@ fun dayoftheweek() {
         else -> "Time has stopped"
     })
 
+}
+
+fun greetingMain(args: Array<String>){
+    val hora = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+
+    println(when (hora){
+        in 1..12 -> "Good Morning"
+        in 12..17 -> "Good Afternoon"
+        in 17..20 -> "Good Evening"
+        else -> "Good Night"
+    })
+
+
+}
+
+fun timeinanotherway(args: Array<String>) {
+    // Check if the user has provided an argument
+    if (args.isNotEmpty()) {
+        // Convert the argument (String) to an Int
+        val time = args[0].toInt()
+
+        // Use a simple if-else statement to check if it's before or after midday
+        println(if (time < 12) "Good morning, Kotlin" else "Good night, Kotlin")
+    } else {
+        println("Please provide a time as an argument.")
+    }
 }
