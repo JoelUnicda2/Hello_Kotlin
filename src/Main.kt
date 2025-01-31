d1adf430fe0b8478f6f68615571107338608fe51
JoelUnicda2 <joel.jimenez@unicda.edu.do>
2025-01-31 06:56:04 -0400

    Practice Dayof week 2

--- src/Main.kt
+++ src/Main.kt
@@ -5,8 +5,20 @@ fun main(args: Array<String>) {
 
     dayoftheweek()
 }
-fun dayoftheweek(dayOfWeek: Int = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
+fun dayoftheweek() {
 
-    println("What day is today? $dayOfWeek")
+    println("What day is today?")
+
+    val day = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+    println( when (day) {
+        1 -> "Sunday"
+        2 -> "Monday"
+        3 -> "Tuesday"
+        4 -> "Wednesday"
+        5 -> "Thursday"
+        6 -> "Friday"
+        7 -> "Saturday"
+        else -> "Time has stopped"
+    })
 
 }
