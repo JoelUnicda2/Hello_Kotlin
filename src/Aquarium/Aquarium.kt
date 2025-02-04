bd7533511cfad7f413314b027bcc11f6c3d64968
JoelUnicda2 <joel.jimenez@unicda.edu.do>
2025-02-04 17:46:32 -0400

    Practica contructor

--- src/Aquarium/Aquarium.kt
+++ src/Aquarium/Aquarium.kt
@@ -1,10 +1,9 @@
 package Aquarium
 
 class Aquarium {
-    var width : Int = 20
-    var  height: Int = 40
+    var height: Int = 40
+    var width: Int = 20
     var length: Int = 100
-
 
     var volume: Int
         get() = width * height * length / 1000
