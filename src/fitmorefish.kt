fc254636c772df342cb6fa7acd627f81128c83fa
JoelUnicda2 <joel.jimenez@unicda.edu.do>
2025-02-04 07:21:27 -0400

    Practica fit

--- src/fitmorefish.kt
+++ src/fitmorefish.kt
@@ -11,5 +11,10 @@ fun canAddFish(
 
 fun main(){
     canAddFish(2.0, listOf(2), 1 , true)
+    canAddFish2(2.0, listOf(2), 1 , true)
+}
 
+
+fun canAddFish2(tankSize: Double, currentFish: List<Int>, fishSize: Int = 2, hasDecorations: Boolean = true): Boolean {
+    return (tankSize * if (hasDecorations) 0.8 else 1.0) >= (currentFish.sum() + fishSize)
 }
