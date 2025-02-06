710bce7f456861abd3c4fe7bfd50ab9f9fdd30e7
JoelUnicda2 <joel.jimenez@unicda.edu.do>
2025-02-06 07:50:24 -0400

    Practica data class container

--- src/Aquarium/Decorations/Decorations.kt
+++ src/Aquarium/Decorations/Decorations.kt
@@ -1,15 +1,16 @@
 package Aquarium.Decorations
 
 fun main (){
-    // Creating some containers with spices
+
+
     val container1 = SpiceContainer("Cinnamon")
     val container2 = SpiceContainer("Paprika")
     val container3 = SpiceContainer("Turmeric")
 
-    // Printing out their labels
-    println(container1.label)  // Output: Container of Cinnamon
-    println(container2.label)  // Output: Container of Paprika
-    println(container3.label)  // Output: Container of Turmeric
+
+    println(container1.label)
+    println(container2.label)
+    println(container3.label)
     makeDecorations()
 
 }
